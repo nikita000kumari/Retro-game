e41eb6ac2585a56aa3cede38601aaf462465a3ca
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2026-06-18 14:39:56 +0530

    oo

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './ReactUI.jsx'
+import initGame from '../ReactComponents/initGame.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+initGame();
